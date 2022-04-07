20338f3c1a6ad800d6df76d2654da70fce394267
tuckl <tuckerlarrabee13@gmail.com>
2022-04-07 16:28:09 -0400

    Initial User model setup

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose');
+const { isEmail } = require('validator');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            validate: [isEmail, 'invalid email']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                //not sure about this field yet
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+})
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+})
+
+const User = model('User', UserSchema);
+
+module.exports = User;
