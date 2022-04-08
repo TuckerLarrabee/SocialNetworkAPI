f442851402515db18e211a4b67c19d8a53a51f66
tuckl <tuckerlarrabee13@gmail.com>
2022-04-07 20:26:19 -0400

    removed unneccesary server code

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-network-api', {
+
+mongoose.connect('mongodb://localhost:27017/social-network-api', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
